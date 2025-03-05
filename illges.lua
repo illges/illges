19d0d6e6f96f50f9f4e8ed76c40ad77e126a889e
illges <brown1ei.eb@gmail.com>
2025-03-04 22:21:51 -0500

    initial commit

---
+++ illges.lua
@@ -0,0 +1,104 @@
+-- illges' default norns script
+
+---@diagnostic disable: undefined-global, lowercase-global, duplicate-set-field
+
+SCRIPT_NAME = "illges"
+local _grid = include 'lib/_grid'
+local _dm = include 'device_manager/lib/device_manager' -- install from https://github.com/illges/device_manager
+
+engine.name = 'PolyPerc'
+
+function init()
+    message = SCRIPT_NAME
+    device_manager = _dm.new({adv=false, debug=false})
+    g=_grid:new()
+
+    screen_redraw_clock()
+    grid_redraw_clock()
+end
+
+function screen_redraw_clock()
+    screen_drawing=metro.init()
+    screen_drawing.time=0.1
+    screen_drawing.count=-1
+    screen_drawing.event=function()
+        if screen_dirty == true then
+            redraw()
+            screen_dirty = false
+        end
+    end
+    screen_drawing:start()
+end
+
+function grid_redraw_clock()
+    grid_drawing=metro.init()
+    grid_drawing.time=0.1
+    grid_drawing.count=-1
+    grid_drawing.event=function()
+        if grid_dirty == true then
+            g:grid_redraw()
+            grid_dirty = false
+        end
+    end
+    grid_drawing:start()
+end
+
+function enc(e, d)
+    if e == 1 then turn(e, d) end
+    if e == 2 then turn(e, d) end
+    if e == 3 then turn(e, d) end
+    screen_dirty = true
+end
+
+function turn(e, d)
+    message = "encoder " .. e .. ", delta " .. d
+end
+
+function key(k, z)
+    if z == 0 then return end
+    if k == 2 then press_down(2) end
+    if k == 3 then press_down(3) end
+    screen_dirty = true
+end
+
+function press_down(i)
+    message = "press down " .. i
+end
+
+function redraw()
+    screen.clear()
+    screen.aa(1)
+    screen.font_face(1)
+    screen.font_size(8)
+    screen.level(15)
+    screen.move(64, 32)
+    screen.text_center(message)
+    screen.pixel(0, 0)
+    screen.pixel(127, 0)
+    screen.pixel(127, 63)
+    screen.pixel(0, 63)
+    screen.fill()
+    screen.update()
+end
+
+function midi_event_note_on(d)
+    message = d.note
+    screen_dirty = true
+end
+
+function midi_event_note_off(d) end
+
+function midi_event_start(d) end
+
+function midi_event_stop(d) end
+
+function midi_event_cc(d) end
+
+function r() ----------------------------- execute r() in the repl to quickly rerun this script
+    norns.script.load(norns.state.script) -- https://github.com/monome/norns/blob/main/lua/core/state.lua
+end
+
+function cleanup() --------------- cleanup() is automatically called on script close
+    clock.cancel(redraw_clock_id) -- melt our clock vie the id we noted
+    clock.cancel(grid_redraw_clock_id) -- melt our clock vie the id we noted
+end
